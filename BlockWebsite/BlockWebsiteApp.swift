4a501c1e9531fafb1aed1efc7ed0ece21d56cae3
Nikhil <nikhil.shirodkar9900@gmail.com>
2024-06-26 14:45:05 +0530

    Initial Commit

---
+++ BlockWebsite/BlockWebsiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockWebsiteApp.swift
+//  BlockWebsite
+//
+//  Created by NIKHIL SHIRODKAR on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BlockWebsiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
