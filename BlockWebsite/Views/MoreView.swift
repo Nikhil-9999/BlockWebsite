f7dca5cc126d3d4032c1a3673ea0e605546d32be
Nikhil <nikhil.shirodkar9900@gmail.com>
2024-06-26 20:00:25 +0530

    [Feature] update list in MoreView

--- BlockWebsite/Views/MoreView.swift
+++ BlockWebsite/Views/MoreView.swift
@@ -9,7 +9,107 @@ import SwiftUI
 
 struct MoreView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                
+                List {
+                    Section {
+                        NavigationLink {
+                            
+                        } label: {
+                            Image(systemName: "person.crop.square.fill")
+                                .padding(10)
+                            Text("Log In or Sign Up")
+                        }
+
+                    }.frame(height: 40)
+                    
+                    Section {
+                        NavigationLink {
+                            //code
+                        } label: {
+                            Image(systemName: "ellipsis.rectangle.fill")
+                                .padding(10)
+                            Text("Password Protection")
+                        }
+                    }.frame(height: 40)
+                    
+                    Section {
+                        
+                        
+                        NavigationLink {
+                            //code
+                        } label: {
+                            Image(systemName: "gearshape.fill")
+                                .padding(10)
+                            Text("Settings")
+                        }
+                        
+                        NavigationLink {
+                            //code
+                        } label: {
+                            Image(systemName: "info.circle")
+                                .padding(10)
+                            Text("About Us")
+                        }
+                        
+                        NavigationLink {
+                            //code
+                        } label: {
+                            Image(systemName: "bubble.right.fill")
+                                .padding(10)
+                            Text("Feedback")
+                        }
+                        
+                        NavigationLink {
+                            //code
+                        } label: {
+                            Image(systemName: "questionmark.circle.fill")
+                                .padding(10)
+                            Text("Contact Us")
+                        }
+                        
+                        NavigationLink {
+                            //code
+                        } label: {
+                            Image(systemName: "heart.fill")
+                                .padding(10)
+                            Text("Share BlockWebsite")
+                        }
+                        
+                        NavigationLink {
+                            //code
+                        } label: {
+                            Image(systemName: "star.fill")
+                                .padding(10)
+                            Text("Rate Us")
+                        }
+                        
+                        NavigationLink {
+                            //code
+                        } label: {
+                            Image(systemName: "arrow.uturn.backward")
+                                .padding(10)
+                            Text("Restore Purchases")
+                        }
+                        
+                        NavigationLink {
+                            //code
+                        } label: {
+                            Image(systemName: "macbook.and.iphone")
+                                .padding(10)
+                            Text("Cross Platform")
+                        }
+                        
+                    }
+                    .cornerRadius(5)
+                }
+                
+            }
+            .navigationTitle("Menu")
+            .navigationBarTitleDisplayMode(.inline)
+            
+        }
     }
 }
 
