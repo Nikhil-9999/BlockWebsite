f3e49327991d0c3bab5b4c77b0375258a5fe08ab
Nikhil <nikhil.shirodkar9900@gmail.com>
2024-06-26 15:02:19 +0530

    [Feature] Update Documentation

--- BlockWebsite/ContentView.swift
+++ BlockWebsite/ContentView.swift
@@ -5,6 +5,25 @@
 //  Created by NIKHIL SHIRODKAR on 26/06/2024.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE
+ [Feature] Description of the feature added
+ 
+ BUG IN THE PRODUCTION
+ [Patch] Description of patch
+ 
+ BUF NOT IN PRODUCTION
+ [Bug] Description of the bug
+ 
+ MUNDANE TASK:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ */
+
 import SwiftUI
 
 struct ContentView: View {
