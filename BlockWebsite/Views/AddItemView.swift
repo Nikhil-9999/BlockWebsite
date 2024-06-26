3f43ace0dee239b6ebe5bf44b6427bea24e9c97a
Nikhil <nikhil.shirodkar9900@gmail.com>
2024-06-26 15:06:14 +0530

    [Feature] add AddItemVIew

---
+++ BlockWebsite/Views/AddItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemView.swift
+//  BlockWebsite
+//
+//  Created by NIKHIL SHIRODKAR on 26/06/2024.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddItemView()
+}
