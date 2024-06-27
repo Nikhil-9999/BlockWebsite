1420c15f0434869669de0b493e122e79da097b36
Nikhil <nikhil.shirodkar9900@gmail.com>
2024-06-27 21:00:55 +0530

    [Feature] update HomeView

--- BlockWebsite/Views/HomeView.swift
+++ BlockWebsite/Views/HomeView.swift
@@ -9,28 +9,22 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-            Text("Your Block List Is Empty !")
-                .font(.title)
-                .fontWeight(.medium)
-                .padding()
-            
-            Text("Tap the button to create block list")
-                .font(.subheadline)
-                .fontWeight(.light)
-                .padding()
-            
-            Button {
-                //code
-            } label: {
-                Text("Block Something")
-                    .frame(width: 150, height: 50)
-                    .background(Color(.cyan))
-                    .cornerRadius(20)
-                    .foregroundStyle(Color(.white))
+        NavigationStack {
+            VStack {
+                Text("Your Block List Is Empty !")
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .padding()
+                
+                Text("Tap the Add Item to create block list")
+                    .font(.subheadline)
+                    .fontWeight(.light)
+                    .padding()
+                
+                
             }
-
         }
+        
         
     }
 }
