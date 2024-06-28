1cb525285a03e043fbbf15d720f743a709f15ef6
Nikhil <nikhil.shirodkar9900@gmail.com>
2024-06-28 20:53:17 +0530

    [feature] add environment objects to popView

--- BlockWebsite/Views/PopupView.swift
+++ BlockWebsite/Views/PopupView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct PopupView: View {
+    @Environment (\.dismiss) var dismiss
+    @Environment (\.modelContext) var context
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
