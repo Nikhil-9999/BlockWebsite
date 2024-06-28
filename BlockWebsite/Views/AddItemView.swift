8da1b21835fc8ac65913938f234a92559806d935
Nikhil <nikhil.shirodkar9900@gmail.com>
2024-06-28 21:08:03 +0530

    [patch] update Plus Button

--- BlockWebsite/Views/AddItemView.swift
+++ BlockWebsite/Views/AddItemView.swift
@@ -6,23 +6,42 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct AddItemView: View {
+    @Environment(\.modelContext) var context
+    @State private var showAdd = false
+    @Query private var items: [AddWebsite]
+    
     var body: some View {
-        VStack {
-            Spacer()
-            
-            Button(action: {
-                
-            }, label: {
-                Image(systemName: "plus.app.fill")
-                    .resizable()
-                    .frame(width: 55, height: 55)
-                    .foregroundStyle(Color(.cyan))
-                    .padding(20)
-            })
-            
-        }
+                VStack {
+                    List {
+                        ForEach(items) { item in
+                            Text("\(item.Web)")
+                        }
+                    }
+                    
+                    
+                    Spacer()
+        
+                    Button(action: {
+                        showAdd.toggle()
+        
+                    }, label: {
+                        Image(systemName: "plus.app.fill")
+                            .resizable()
+                            .frame(width: 55, height: 55)
+                            .foregroundStyle(Color(.cyan))
+                            .padding(20)
+                    })
+                    .sheet(isPresented: $showAdd, content: {
+                        NavigationStack {
+                            PopupView()
+                        }
+                        .presentationDetents([.height(200)])
+                    })
+        
+                }
     }
 }
 
