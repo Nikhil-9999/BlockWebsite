e437cb4a0e2ef7c7ab032a4f518d95667608c014
Nikhil <nikhil.shirodkar9900@gmail.com>
2024-06-28 20:49:48 +0530

    [feature] add PopupView to project

---
+++ BlockWebsite/Views/PopupView.swift
@@ -0,0 +1,18 @@
+//
+//  PopupView.swift
+//  BlockWebsite
+//
+//  Created by NIKHIL SHIRODKAR on 28/06/2024.
+//
+
+import SwiftUI
+
+struct PopupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopupView()
+}
