ba909cc1af4a4580fc6927da168cf09893246d1f
Nikhil <nikhil.shirodkar9900@gmail.com>
2024-06-30 15:52:59 +0530

    [feature] add ContentBlocker extension

---
+++ ContentBlocker/ContentBlockerRequestHandler.swift
@@ -0,0 +1,22 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  ContentBlocker
+//
+//  Created by NIKHIL SHIRODKAR on 30/06/2024.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
